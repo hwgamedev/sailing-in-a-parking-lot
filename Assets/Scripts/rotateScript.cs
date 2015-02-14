fc8cfadca47469b9646cc5f117272e1d838e2016
Tom Robertson <tomasrobertson@ymail.com>
2015-02-14 20:33:33 +0000

    rotation

--- Assets/Scripts/rotateScript.cs
+++ Assets/Scripts/rotateScript.cs
@@ -10,6 +10,8 @@ public class rotateScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		transform.rotation = Quaternion.identity;
+		transform.Rotate (new Vector3 (0, 0, 1), Input.gyro.attitude.z);
+
 	}
 }
