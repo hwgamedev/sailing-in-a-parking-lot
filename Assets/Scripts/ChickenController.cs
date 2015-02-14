e0f0f329c8db3364455af69181c0a5bbb3f9f542
mindaugaskadzys <mindaugas.kadzys@gmail.com>
2015-02-14 23:29:33 +0000

    Chicken Ultimate controller version 5.infinity

--- Assets/Scripts/ChickenController.cs
+++ Assets/Scripts/ChickenController.cs
@@ -8,6 +8,12 @@ public class ChickenController : MonoBehaviour {
 	public float jumpSpeed = 15f;    
 	public float airSpeedMultiplier = .3f;
 	public bool standing;
+
+	//checker for ground
+	public Transform toGroundL;
+	public Transform toGroundR;
+
+
 	private ChickenMovement controller;
 	private Animator animator;
 
@@ -21,21 +27,22 @@ public class ChickenController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		Vector3 followL = toGroundL.transform.position;
+		Vector3 followR = toGroundR.transform.position;
+		Debug.DrawLine(transform.position, followL, Color.green);
+		Debug.DrawLine(transform.position, followR, Color.green);
+
+		
+		bool standingPrev = standing;
+		if(!checkGrounded (followL) && !checkGrounded (followR) && standingPrev)
+			standing = false;
 	
 		var forceX = 0f;
 		var forceY = 0f;
 		
 		var absVelocityX = Mathf.Abs(rigidbody2D.velocity.x);
 		var absVelocityY = Mathf.Abs(rigidbody2D.velocity.y);
-		
-		if (absVelocityY < 0.2f)
-		{
-			standing = true;
-		}
-		else
-		{
-			standing = false;
-		}
 		
 		if (controller.moving.x != 0)
 		{
@@ -58,15 +65,36 @@ public class ChickenController : MonoBehaviour {
 			{
 				forceY = jumpSpeed * controller.moving.y;
 				rigidbody2D.AddForce(new Vector2(0, forceY), ForceMode2D.Impulse);
+				animator.SetInteger("animState", 2);
 
 			}
-			animator.SetInteger("animState", 2);
 		}
-		else if (absVelocityY > 0)
-		{
-			animator.SetInteger("animState", 3);
+
+		if(!standing) {
+			if (rigidbody2D.velocity.y < 0)
+			{
+				animator.SetInteger("animState", 3);
+			}
+			else
+				animator.SetInteger ("animState",2);
+
 		}
 		
 		rigidbody2D.AddForce(new Vector2(forceX, 0));
+	}
+
+	bool checkGrounded(Vector3 endOfLine) {
+		bool standingChanged = false;
+		//RaycastHit hit;
+		RaycastHit2D[] hits = Physics2D.LinecastAll (transform.position, endOfLine);
+
+		for(var i = 0; i < hits.Length; i++) {
+			if((hits[i].collider.tag != "Player")) {
+				standing = true;
+				standingChanged = true;
+			}
+		}
+
+		return standingChanged;
 	}
 }
