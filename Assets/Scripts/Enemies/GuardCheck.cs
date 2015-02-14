fbf7a9d3e278fe51a86fa8f2630a7b1ba0e2a07a
Tamara Tanasescu <gokimster@yahoo.com>
2015-02-14 23:34:33 +0000

    Water Enemy and Guard script for detecting player

---
+++ Assets/Scripts/Enemies/GuardCheck.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuardCheck : MonoBehaviour {
+
+	public float sightDistance = 5f;
+	public bool sawPlayer = false;
+	//public Transform player;
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+		CheckForPlayer();
+		DebugRays();
+	}
+
+	void CheckForPlayer()
+	{
+		//RaycastHit2D hit;
+		if (gameObject.GetComponent<Patrol>().facingRight){
+			sawPlayer = Physics2D.Raycast(new Vector2(transform.position.x+1,transform.position.y), Vector2.right, sightDistance);
+		}else
+			sawPlayer = Physics2D.Raycast(new Vector2(transform.position.x-1,transform.position.y), -Vector2.right, sightDistance);
+	}
+
+	void DebugRays()
+	{
+		if (gameObject.GetComponent<Patrol>().facingRight){
+			Debug.DrawRay (transform.position,Vector3.right * sightDistance, Color.red);
+		}else
+			Debug.DrawRay (transform.position,Vector3.left * sightDistance, Color.red);
+	}
+
+
+}
