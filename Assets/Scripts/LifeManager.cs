8d000e7a064d15aa0f552a40e551207d0a171ef0
John Scott <johnz0r.steam@gmail.com>
2015-02-15 10:45:51 +0000

    Explode, sounds, head rocket

--- Assets/Scripts/LifeManager.cs
+++ Assets/Scripts/LifeManager.cs
@@ -6,12 +6,15 @@ public class LifeManager : MonoBehaviour {
 	public float invincibilityTime = 3;
 		
 	private float invincibilityRemaining;
-	private int health = 3;
+	public int health = 3;
 	private SpriteRenderer sr;
 	private float blinkyInterval;
 	private float blinkyIntervalMax = 0.1f;
 	private HeartManager hm;
     private GameObject[] chunks = new GameObject[6];
+    private bool alive = true;
+    public AudioClip BWUAK;
+    public AudioClip hit;
 
 	public bool incHealth() {
 		if (health < 3) {
@@ -26,7 +29,7 @@ public class LifeManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        chunks = Resources.LoadAll<GameObject>("Prefabs/Explode");
+        chunks = Resources.LoadAll<GameObject>("Prefabs/ChickExplode");
 		hm = GameObject.FindGameObjectWithTag ("healthbar").GetComponent<HeartManager> ();
 		invincibilityRemaining = invincibilityTime;
 		sr = GetComponent<SpriteRenderer> ();
@@ -35,10 +38,9 @@ public class LifeManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (health ==0)
+        if (alive)
         {
-            die();
-        }
+
 		if(invincibilityRemaining > 0) {
 			invincibilityRemaining -= Time.deltaTime;
 			Color c = sr.color;
@@ -55,53 +57,47 @@ public class LifeManager : MonoBehaviour {
 			c.a = c.a == 0.2f? 1.0f : 1.0f;
 			sr.color = c;
 		}
+
+        }
 	}
 
     void OnCollisionEnter2D(Collision2D col)
     {
         if ((col.gameObject.tag == "Enemy" || col.gameObject.tag == "Projectile") && health > 0 && invincibilityRemaining == 0)
         {
-            if (col.gameObject.tag == "Enemy" && health > 0 && invincibilityRemaining == 0)
-            {
                 health--;
+                AudioSource.PlayClipAtPoint(hit, transform.position);
                 hm.updateHearts(health);
+                if (health == 0)
+                {
+                    die();
+                }
                 invincibilityRemaining = invincibilityTime;
                 float xDirection = System.Math.Sign(rigidbody2D.velocity.x);
                 rigidbody2D.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
-            }
+            
+
         }
     }
-
-	public bool incHealth() {
-		if (health < 3) {
-			health++;
-			hm.updateHearts(health);
-			return true;
-		}
-		return false;
-	}
-
-	public int getHealth() { return health; }
 
     public void die()
     {
-        for (int i = 0; i < chunks.Length; i++)
+        alive = false;
+        AudioSource.PlayClipAtPoint(BWUAK, transform.position);
+        print("Chunk");
+        GameObject chunkhead = Instantiate(chunks[0], transform.position, Quaternion.identity) as GameObject;
+        chunkhead.rigidbody2D.AddForce(Vector3.right * Random.Range(-5, 5));
+        chunkhead.rigidbody2D.AddForce(Vector3.up * Random.Range(300, 1000));
+        for (int i = 1; i < 6; i++)
         {
-            print("Chunk");
+            
             GameObject chunk = Instantiate(chunks[i], transform.position, Quaternion.identity) as GameObject;
-            chunks[i].rigidbody2D.AddForce(Vector3.right * Random.Range(-25, 25));
-            chunks[i].rigidbody2D.AddForce(Vector3.up * Random.Range(50, 200));
+            chunk.rigidbody2D.AddForce(Vector3.right * Random.Range(-25, 25));
+            chunk.rigidbody2D.AddForce(Vector3.up * Random.Range(50, 200));
         }
+
         Destroy(gameObject);
         
     }
-	void OnCollisionEnter2D(Collision2D col) {
-		if ((col.gameObject.tag == "Enemy" || col.gameObject.tag == "Projectile") && health > 0 && invincibilityRemaining == 0) {
-			health--;
-			hm.updateHearts(health);
-			invincibilityRemaining = invincibilityTime;
-			float xDirection = System.Math.Sign (rigidbody2D.velocity.x);
-			rigidbody2D.AddForce (new Vector2(0,5),ForceMode2D.Impulse);
-		}
-	}
+
 }
