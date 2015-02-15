a39ec7d1ac088b2c18919aeb24ff8d6b55cad2c9
Tamara Tanasescu <gokimster@yahoo.com>
2015-02-15 00:56:49 +0000

    Fire Enemy launches fireballs when he sees player

---
+++ Assets/Scripts/Enemies/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	public bool goRight;
+	public float maxShootSpeed = 100f;
+	public float minShootSpeed = 80f;
+	void Start () {
+		rigidbody2D.AddTorque (-15);
+		ForceApply ();
+	}
+
+	void Update () {
+	
+	}
+
+	void ForceApply ()
+	{
+		if (goRight)
+			rigidbody2D.AddForce(Vector3.right * Random.Range(minShootSpeed, maxShootSpeed));
+		else
+			rigidbody2D.AddForce(Vector3.left * Random.Range(minShootSpeed, maxShootSpeed));
+	}
+
+	void OnCollisionEnter2D ()
+	{
+		GameObject.Destroy (gameObject);
+	}
+}
