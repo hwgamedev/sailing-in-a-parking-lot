d10f0b528eca9f6b717297514122dc2b27c97b42
John Scott <johnz0r.steam@gmail.com>
2015-02-15 01:54:37 +0000

    2D Magic

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -2,62 +2,27 @@
  using System.Collections;
  
  public class Drag : MonoBehaviour {
-     private float dist;
-     private bool dragging = false;
+
+     private Vector3 screenPoint;
      private Vector3 offset;
-     private Transform toDrag;
-     bool deleted = false;
+     public GameObject combiner;
 
-     void Update()
+     void OnMouseDown()
      {
-         Vector3 v3;
 
-         if (Input.touchCount != 1)
-         {
-             dragging = false;
-             return;
-         }
+             offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
 
-         Touch touch = Input.touches[0];
-         Vector3 pos = touch.position;
-         if (!deleted)
-         {
-         if (touch.phase == TouchPhase.Began)
-         {
-             RaycastHit hit;
-             Ray ray = Camera.main.ScreenPointToRay(pos);
-
-                 if (Physics.Raycast(ray, out hit) && (hit.collider.tag == "Draggable"))
-                 {
-                     toDrag = hit.transform;
-                     dist = hit.transform.position.z - Camera.main.transform.position.z;
-                     v3 = new Vector3(pos.x, pos.y, dist);
-                     v3 = Camera.main.ScreenToWorldPoint(v3);
-                     offset = toDrag.position - v3;
-                     dragging = true;
-                 }
-             }
-             if (dragging && touch.phase == TouchPhase.Moved)
-             {
-                 v3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
-                 v3 = Camera.main.ScreenToWorldPoint(v3);
-                 toDrag.position = v3 + offset;
-             }
-             if (dragging && (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled))
-             {
-                 dragging = false;
-             }
-         }
      }
 
-     
-
-     void OnCollisionEnter(Collision col)
+     void OnMouseDrag()
      {
-         if (col.gameObject.tag == "Dropped")
-         {
-             deleted = true;
-             Destroy(gameObject);
-         }
+
+             Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+             Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+             transform.position = curPosition;
+
+     }
+     void Update()
+     {
      }
  }
--- Assets/Scripts/MagicCombiner.cs
+++ Assets/Scripts/MagicCombiner.cs
@@ -1,15 +1,76 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MagicCombiner : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+public class MagicCombiner : MonoBehaviour
+{
+    public bool earthOn = false;
+    public bool attackOn = false;
+
+    private SpriteRenderer spriteRenderer;
+    private GameObject[] tokens = new GameObject[2];
+    private GameObject[] _tokens = new GameObject[2];
+    private Sprite[] sprites;
+
+
+    void Start()
+    {
+        tokens = Resources.LoadAll<GameObject>("Prefabs/Tokens");
+        for (int i = 0; i < tokens.Length; i++ )
+        {
+            _tokens[i] = Instantiate(tokens[i]) as GameObject;
+        }
+        sprites = Resources.LoadAll<Sprite>("Sprites/MagicComb/Combiner");
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer.sprite == null)
+            spriteRenderer.sprite = sprites[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkCollisions();
+    }
+
+    void checkCollisions()
+    {
+        if (!earthOn)
+        {
+            if (renderer.bounds.Intersects(_tokens[0].renderer.bounds))
+            {
+                earthOn = true;
+                updateSprite();
+                Destroy(_tokens[0]);
+            }
+        }
+        if (!attackOn)
+        {
+            if (renderer.bounds.Intersects(_tokens[1].renderer.bounds))
+            {
+                attackOn = true;
+                updateSprite();
+                Destroy(_tokens[1]);
+            }
+        }
+    }
+
+    void updateSprite()
+    {
+        if(earthOn && attackOn)
+        {
+            spriteRenderer.sprite = sprites[1];
+        }
+        else if (earthOn)
+        {
+            spriteRenderer.sprite = sprites[2];
+        }
+        else if (attackOn)
+        {
+            spriteRenderer.sprite = sprites[3];
+        }
+        else
+        {
+            spriteRenderer.sprite = sprites[0];
+        }
+    }
 }
