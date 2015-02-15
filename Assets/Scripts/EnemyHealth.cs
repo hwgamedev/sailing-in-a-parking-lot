6d31ffe7c0a8a3e2a27d6ef9fac9942d7a042a0c
John Scott <johnz0r.steam@gmail.com>
2015-02-15 12:18:44 +0000

    Knockback fix

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -56,7 +56,7 @@ public class EnemyHealth : MonoBehaviour {
         rigidbody2D.AddForce(new Vector2(knockback, 5f), ForceMode2D.Impulse);
         healthBar.GetComponentInChildren<EnemyHealthBar>().updatePercentage(totalHealth / maxHealth * 1.0f);
 
-        if (totalHealth < 0)
+        if (totalHealth <= 0)
         {
             GetComponent<DropItem>().dropItem();
                 explode();
